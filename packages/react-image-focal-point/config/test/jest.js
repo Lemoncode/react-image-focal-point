594aea948f5e16e91215fe2fe4db7b5eedf9ca6c
daniel.sanchez <daniel.sanchez@lemoncode.net>
2023-02-03 15:21:26 +0100

    add sourcemaps in dev mode

--- packages/react-image-focal-point/config/test/jest.js
+++ packages/react-image-focal-point/config/test/jest.js
@@ -1,12 +1,20 @@
-module.exports = {
+export default {
   rootDir: '../../',
   verbose: true,
-  preset: 'ts-jest',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  extensionsToTreatAsEsm: ['.ts'],
   restoreMocks: true,
   testEnvironment: 'jsdom',
   moduleDirectories: ['<rootDir>/src', 'node_modules'],
   setupFilesAfterEnv: ['<rootDir>/config/test/setup.ts'],
   moduleNameMapper: {
-    "\\.css$": "<rootDir>/config/test/file.mock.js",
+    '\\.css$': '<rootDir>/config/test/file.mock.js',
   },
 };
