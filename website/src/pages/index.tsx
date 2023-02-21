717bfc34fa923694fd3459edeb8cb9e00ed81608
daniel.sanchez <daniel.sanchez@lemoncode.net>
2023-02-21 14:32:42 +0100

    apply feedback

---
+++ website/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import { Header } from '@site/src/components';
+
+const Home: React.FC = () => {
+  return (
+    <Layout description="React Image Focal Point provides a simple way to center a cropped image on a specific point.">
+      <Header />
+      <main>
+        <p>TODO: Add youtube video</p>
+      </main>
+    </Layout>
+  );
+};
+
+export default Home;
